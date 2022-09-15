0c1f362d95a985f0815fe663f98eec50c91f889d
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-09-15 15:04:13 +0200

    Added Posts

--- functions.php
+++ functions.php
@@ -80,3 +80,10 @@ function my_nav_css_stylesheet()
     wp_enqueue_style('custom-nav', get_stylesheet_directory_uri() . '/style-nav.css');
 }
 add_action('wp_enqueue_scripts', 'my_nav_css_stylesheet');
+
+function load_bootstrap()
+{
+    wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css");
+    wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js", null, null, true);
+}
+add_action("wp_enqueue_scripts", "load_bootstrap");
---
+++ template-parts/content/entry_content.php
@@ -0,0 +1,6 @@
+<div class="col-12 d-flex justify-content-center flex-wrap entry-container">
+	<img class="py-2 news-image" src="<?php the_field("news_image") ?>" alt="">
+	<div class="col-10 py-2">
+		<p><?php the_field("body_text") ?></p>
+	</div>
+</div>
